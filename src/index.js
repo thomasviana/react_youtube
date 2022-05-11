9c96ecc3422ee34d7acf9fb31daef7b5ab04788d
Thomas Viana <thomasviana16@gmail.com>
2022-05-11 16:42:13 -0500

    Add video detail and allow video selection.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from 'youtube-api-search';
 import SearchBar from "./components/search_bar";
+import VideoDetail from "./components/video_detail";
 import VideoList from "./components/video_list";
+
 const API_KEY = 'AIzaSyDyM770yoyBUelI4tXnjKJT6yM4dCKuTuE';
 
 
@@ -12,10 +14,16 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] }
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        }
 
         YTSearch({key: API_KEY, term: 'surfborads'}, (data) => {
-            this.setState({ videos: data });
+            this.setState({ 
+                videos: data , 
+                selectedVideo: data[0]
+            });
         });
     }
     
@@ -23,7 +31,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} />
             </div>
          );
     }
